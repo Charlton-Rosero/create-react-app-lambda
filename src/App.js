b541ae63d722608247ce5f008928bc7b5230e4d9
Charlton_Rosero <charltonprosero@gmail.com>
2022-04-15 10:23:07 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,62 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+import React from 'react';
+import './App.css';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import Zodiac from './components/Zodiac';
+import TimeFrame from './components/TimeFrame';
+import History from './components/History';
+import Footer from './components/Footer';
 
-  handleClick = api => e => {
-    e.preventDefault()
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
 
-  render() {
-    const { loading, msg } = this.state
+function App() {
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  React.useEffect(() => {
+    const json = localStorage.getItem("site-dark-mode");
+    const currentMode = JSON.parse(json);
+    if (currentMode) {
+      setDarkMode(true);
+    } else {
+      setDarkMode(false);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+    const json = JSON.stringify(darkMode);
+    localStorage.setItem("site-dark-mode", json);
+  }, [darkMode]);
+
+  return (
+    <div className="App">
+      <nav className="navbar">
+          <h1>Horoscopia</h1> 
+          <NavLink to="/"> Home </NavLink>
+          <NavLink to="/zodiac"> Zodiac </NavLink>
+          <NavLink to="/history"> History </NavLink>
+          <NavLink to="/about"> About Us </NavLink>
+          <button className = 'toggleDarkModeButton' onClick={() => setDarkMode(!darkMode)}></button> 
+      </nav>    
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Footer />} /> */}
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/zodiac" element={<Zodiac />} />
+        <Route path="/time" element={<TimeFrame />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+      <Footer/>
+    </div>
+  );
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
+export default App;
