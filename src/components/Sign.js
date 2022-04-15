28d094a3891bad579fed81aa49ef33e58263b91e
Charlton_Rosero <charltonprosero@gmail.com>
2022-04-15 12:08:09 -0400

    Fixed??

--- src/components/Sign.js
+++ src/components/Sign.js
@@ -8,8 +8,21 @@ function Sign({handleSelectedSign}){
 
     const [signs, setSigns] = useState([])  
    
+    const sign = [  "aries",
+    "taurus",
+    "gemini",
+    "cancer",
+    "leo",
+    "virgo",
+    "libra",
+    "scorpio",
+    "sagittarius",
+    "capricorn",
+    "aquarius",
+    "pisces" ]   
+
     useEffect(()=>{
-        getSigns().then(setSigns)
+        setSigns(sign)
     },[])
 
     return(
